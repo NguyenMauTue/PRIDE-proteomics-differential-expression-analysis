72618df6e9674270b60afb8bf03a64f0a6d2a152
Nguyen Mau Tue <91324335+NguyenMauTue@users.noreply.github.com>
2026-02-20 14:57:04 +0700

    Update 02_exploratory_analysis_PCA.R

--- 02_exploratory_analysis_PCA.R
+++ 02_exploratory_analysis_PCA.R
@@ -45,10 +45,59 @@ plot = ggplot(pca_df, aes(x = PC1, y = PC2, color = Group)) +
   labs(title = "PCA reveals global proteomic differences between tumor and normal samples",
        x = paste0("PC1 (", percentVar[1], "% variance)"),
        y = paste0("PC2 (", percentVar[2], "% variance)")) 
-ggsave("plots/PCA_plot.png", width = 6, height = 5, dpi = 300)
+ggsave("results/PCA_plot.png", width = 6, height = 5, dpi = 300)
 
 #Statistical test: whether PC1 differs between tumor and normal samples
 t.test(PC1 ~ Group, data = pca_df)
 
 #Loading processing
+loadings <- as.data.frame(pca_res$rotation)
+loadings$ProteinID <- rownames(loadings)
 
+#Map gene names
+loadings$GeneID <- proteinMetaData$GeneNames[
+  match(loadings$ProteinID,
+        proteinMetaData$ProteinIDS)
+]
+#Rank proteins by absolute contribution to PC1
+loadings$abs_PC1 <- abs(loadings$PC1)
+top_PC1_proteins <- loadings[
+  order(loadings$abs_PC1, decreasing = TRUE),
+]
+
+#Saving results
+write.csv(
+  top_PC1_proteins,
+  file = "data/top_PC1_loadings_ranked.csv",
+  row.names = FALSE
+)
+
+#Select top 40 proteins
+heatmap_proteins <- top_PC1_proteins$ProteinID[1:40]
+heatmap_matrix <- expMatrixNorm[
+  match(heatmap_proteins,
+        rownames(expMatrixNorm)),
+]
+
+#Sample annotation
+annotation_col <- data.frame(Group = group)
+rownames(annotation_col) <- colnames(heatmap_matrix)
+
+#Plot heatmaps
+pheatmap(
+  heatmap_matrix,
+  annotation_col = annotation_col,
+  scale = "row",
+  clustering_distance_rows = "euclidean",
+  clustering_method = "ward.D2",
+  show_colnames = FALSE,
+  show_rownames = TRUE,
+  main = paste(
+    "Heatmap of Top",
+    top_n,
+    "Proteins Contributing to PC1"
+  ),
+  color = colorRampPalette(
+    c("blue", "white", "red")
+  )(100)
+)
