9bc7f8ea4277d6bba303769f1f78b78654481363
Nguyen Mau Tue <91324335+NguyenMauTue@users.noreply.github.com>
2026-03-10 23:03:55 +0700

    Update 11_Network-expression_integration.R

--- 11_Network-expression_integration.R
+++ 11_Network-expression_integration.R
@@ -3,45 +3,59 @@
 ############################################################
 
 library(biomaRt)
-
-mart <- useMart("ensembl", dataset="hsapiens_gene_ensembl")
-
-mapping <- getBM(
-  attributes = c("hgnc_symbol", "uniprotswissprot"),
-  filters = "hgnc_symbol",
-  values = network_summary$Symbol,
-  mart = mart
-)
-
-colnames(mapping) <- c("SYMBOL","UNIPROT")
-
-mapping <- mapping[mapping$UNIPROT != "", ]
-mapping <- mapping[!duplicated(mapping$SYMBOL), ]
-
-network_summary2 <- merge(
-  network_summary,
-  mapping,
-  by.x="Symbol",
-  by.y="SYMBOL"
-)
-
-
-############################################################
-# Merge with limma results
-############################################################
-
-limmanetwork_df = merge(
-  network_summary2,
-  imputed_result,
-  by = "UNIPROT"
-)
-
 library(dplyr)
 
-limmanetwork_df = limmanetwork_df %>%
-  relocate(Symbol, UNIPROT)
+network_summary =
+  read.csv("../results/network_summary.csv")
+
+imputed_result =
+  read.csv("../results/imputed_result.csv")
+
+
+mart =
+  useMart("ensembl",
+          dataset="hsapiens_gene_ensembl")
+
+mapping =
+  getBM(
+    attributes=c(
+      "hgnc_symbol",
+      "uniprotswissprot"
+    ),
+    filters="hgnc_symbol",
+    values=network_summary$Symbol,
+    mart=mart
+  )
+
+colnames(mapping)=
+  c("SYMBOL","UNIPROT")
+
+mapping =
+  mapping[mapping$UNIPROT!="", ]
+
+mapping =
+  mapping[!duplicated(mapping$SYMBOL), ]
+
+
+network_summary2 =
+  merge(
+    network_summary,
+    mapping,
+    by.x="Symbol",
+    by.y="SYMBOL"
+  )
+
+
+limmanetwork_df =
+  merge(
+    network_summary2,
+    imputed_result,
+    by="UNIPROT"
+  )
+
 
 write.csv(
   limmanetwork_df,
-  "../results/limma_network_table.csv"
+  "../results/limma_network_table.csv",
+  row.names=FALSE
 )
